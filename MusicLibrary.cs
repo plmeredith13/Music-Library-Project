7db27772b31191ae4f5a463855e39ad916d4e193
plmeredith13 <plmeredith13@gmail.com>
2020-05-05 23:52:39 -0400

    Added Sorting, Shuffling, and SubMenus to library

--- MusicLibrary.cs
+++ MusicLibrary.cs
@@ -42,12 +42,18 @@ namespace plmeredith13.CodeLou.FinalProject
                         DisplaySongs();
                         break;
                     case 3:
-                        SearchSongs();
+                        SearchList();
                         break;
                     case 4:
-                        RemoveSongs();
+                        SortList();
                         break;
                     case 5:
+                        Shuffle(SongsList);
+                        break;
+                    case 6:
+                        RemoveSongs();
+                        break;
+                    case 7:
                         inputtingSong = false;
                         break;
                 }
@@ -59,9 +65,9 @@ namespace plmeredith13.CodeLou.FinalProject
             if (Songs.Any())
             {
                 Console.WriteLine($"\n\"Title\"  -  Artist   (Album) \n");
-                SongsList.ForEach(x =>
+                SongsList.ForEach(Song =>
                 {
-                    Console.WriteLine(x.SongDisplay + "\n");
+                    Console.WriteLine(Song.SongDisplay + "\n");
                 });
             }
             else
@@ -73,22 +79,118 @@ namespace plmeredith13.CodeLou.FinalProject
 
         private static void DisplaySongs() => DisplaySongs(SongsList);
 
+        private static void SearchList()
+        {
+            SearchMenu();
+            var option = Console.ReadLine();
+
+                switch (int.Parse(option))
+                {
+                    case 1:
+                        SearchSongs();
+                        break;
+                    case 2:
+                        SearchArtists();
+                        break;
+                    case 3:
+                        SearchAlbums();
+                        break;
+                }
+        }
+
         private static void SearchSongs()
         {
             Console.WriteLine("\nSearch for a song: \n");
             var searchString = Console.ReadLine();
-            var searchResults = SongsList.Where(x => x.SongTitle.ToLower().Contains(searchString.ToLower()));
+            var searchResults = SongsList.Where(Song => Song.SongTitle.ToLower().Contains(searchString.ToLower())).ToList();
             DisplaySongs(searchResults);
         }
 
-        private static void DisplayMenu()
+        private static void SearchArtists()
         {
-            Console.WriteLine("Select from the following operations:");
-            Console.WriteLine("1: Enter new song");
-            Console.WriteLine("2: List all songs");
-            Console.WriteLine("3: Search for song by title");
-            Console.WriteLine("4: Remove a song by title");
-            Console.WriteLine("5: Exit \n");
+            Console.WriteLine("\nSearch for an artist: \n");
+            var searchString = Console.ReadLine();
+            var searchResults = SongsList.Where(Song => Song.Artist.ToLower().Contains(searchString.ToLower())).ToList();
+            DisplaySongs(searchResults);
+        }
+
+        private static void SearchAlbums()
+        {
+            Console.WriteLine("\nSearch for an album: \n");
+            var searchString = Console.ReadLine();
+            var searchResults = SongsList.Where(Song => Song.Album.ToLower().Contains(searchString.ToLower())).ToList();
+            DisplaySongs(searchResults);
+        }
+
+        private static void SearchMenu()
+        {
+            Console.WriteLine("\nHow would you like to search the list?");
+            Console.WriteLine("1: Search by song title");
+            Console.WriteLine("2: Search by artist");
+            Console.WriteLine("3: Search by album");
+            Console.WriteLine(" ");
+        }
+
+        private static Random random = new Random();
+        
+        private static void Shuffle<Song>(IList<Song> SongsList)
+        {
+            Console.WriteLine("\nHere's your randomized songlist! \n");
+            int n = SongsList.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = random.Next(n + 1);
+                Song value = SongsList[k];
+                SongsList[k] = SongsList[n];
+                SongsList[n] = value;
+            }
+            DisplaySongs();
+        }
+
+        private static void SortList()
+        {
+            SortMenu();
+            var option = Console.ReadLine();
+
+                switch (int.Parse(option))
+                {
+                    case 1:
+                        SortByTitle();
+                        break;
+                    case 2:
+                        SortByArtist();
+                        break;
+                    case 3:
+                        SortByAlbum();
+                        break;
+                }
+        }
+
+        private static void SortByTitle()
+        {
+            SongsList.Sort((x, y) => x.SongTitle.CompareTo(y.SongTitle));
+            DisplaySongs();
+        }
+
+        private static void SortByArtist()
+        {
+            SongsList.Sort((x, y) => x.Artist.CompareTo(y.Artist));
+            DisplaySongs();
+        }
+
+        private static void SortByAlbum()
+        {
+            SongsList.Sort((x, y) => x.Album.CompareTo(y.Album));
+            DisplaySongs();
+        }
+
+        private static void SortMenu()
+        {
+            Console.WriteLine("\nHow would you like to sort the list?");
+            Console.WriteLine("1: Sort by song title");
+            Console.WriteLine("2: Sort by artist");
+            Console.WriteLine("3: Sort by album");
         }
 
         private static void RemoveSongs()
@@ -138,6 +240,18 @@ namespace plmeredith13.CodeLou.FinalProject
 
             SongsList.Add(Song);
             Save();
+        }
+
+        private static void DisplayMenu()
+        {
+            Console.WriteLine("Select from the following operations:");
+            Console.WriteLine("1: Enter new song");
+            Console.WriteLine("2: List all songs");
+            Console.WriteLine("3: Search for a song");
+            Console.WriteLine("4: Sort the song library");
+            Console.WriteLine("5: Shuffle the song library");
+            Console.WriteLine("6: Remove a song by title");
+            Console.WriteLine("7: Exit \n");
         }
 
     }
--- Song.cs
+++ Song.cs
@@ -1,4 +1,5 @@
 using System;
+
 namespace src
 {
     public class Song
