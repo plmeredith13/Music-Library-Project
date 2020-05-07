375a48041c1bede44d4bfca8b15e0f32ee1cd956
plmeredith13 <plmeredith13@gmail.com>
2020-05-06 22:09:28 -0400

    As good as it's gonna get I guess...

--- MusicLibrary.cs
+++ MusicLibrary.cs
@@ -20,7 +20,7 @@ namespace plmeredith13.CodeLou.FinalProject
         }
         static List<Song> Read()
         {
-            return JsonSerializer.Deserialize<List<Song>>(File.ReadAllText(_songRepositoryPath));
+            return JsonSerializer.Deserialize<List<Song>>(File.ReadAllText(_songRepositoryPath));   //this throws an exception once the json file hits 1KB and i've spent hours researching it and can't figure out why
         }
 
         static void Main(string[] args)
@@ -64,7 +64,7 @@ namespace plmeredith13.CodeLou.FinalProject
         {
             if (Songs.Any())
             {
-                Console.WriteLine($"\n\"Title\"  -  Artist   (Album) \n");
+                Console.WriteLine($"\n\"Title\"  -  Artist   (Album) \n");              //displays the full library of songs stored in the json file
                 SongsList.ForEach(Song =>
                 {
                     Console.WriteLine(Song.SongDisplay + "\n");
@@ -102,24 +102,24 @@ namespace plmeredith13.CodeLou.FinalProject
         {
             Console.WriteLine("\nSearch for a song: \n");
             var searchString = Console.ReadLine();
-            var searchResults = SongsList.Where(Song => Song.SongTitle.ToLower().Contains(searchString.ToLower())).ToList();
-            DisplaySongs(searchResults);
+            var searchResults = SongsList.Where(Song => Song.SongTitle.ToLower().Contains(searchString.ToLower())).ToList();    //this filters the list correctly but...
+            DisplaySongs(searchResults);                                                                                        //...this doesn't display the filtered list for some reason (also spent hours researching this, tried about 15 different ways around it to no avail)
         }
 
         private static void SearchArtists()
         {
             Console.WriteLine("\nSearch for an artist: \n");
             var searchString = Console.ReadLine();
-            var searchResults = SongsList.Where(Song => Song.Artist.ToLower().Contains(searchString.ToLower())).ToList();
-            DisplaySongs(searchResults);
+            var searchResults = SongsList.Where(Song => Song.Artist.ToLower().Contains(searchString.ToLower())).ToList();       //as already mentioned, this filters the list correctly but...
+            DisplaySongs(searchResults);                                                                                        //...this STILL doesn't display the filtered list for some reason
         }
 
         private static void SearchAlbums()
         {
             Console.WriteLine("\nSearch for an album: \n");
             var searchString = Console.ReadLine();
-            var searchResults = SongsList.Where(Song => Song.Album.ToLower().Contains(searchString.ToLower())).ToList();
-            DisplaySongs(searchResults);
+            var searchResults = SongsList.Where(Song => Song.Album.ToLower().Contains(searchString.ToLower())).ToList();        //once again, this filters the list correctly but...
+            DisplaySongs(searchResults);                                                                                        //...this STILL doesn't display the filtered list for some reason
         }
 
         private static void SearchMenu()
@@ -135,7 +135,7 @@ namespace plmeredith13.CodeLou.FinalProject
         
         private static void Shuffle<Song>(IList<Song> SongsList)
         {
-            Console.WriteLine("\nHere's your randomized songlist! \n");
+            Console.WriteLine("\nHere's your randomized songlist! \n");             //shuffles the full library of songs stored in the json file
             int n = SongsList.Count;
             while (n > 1)
             {
@@ -169,19 +169,19 @@ namespace plmeredith13.CodeLou.FinalProject
 
         private static void SortByTitle()
         {
-            SongsList.Sort((x, y) => x.SongTitle.CompareTo(y.SongTitle));
+            SongsList.Sort((x, y) => x.SongTitle.CompareTo(y.SongTitle));           //sorts the full library of songs by song title in alphabetical order
             DisplaySongs();
         }
 
         private static void SortByArtist()
         {
-            SongsList.Sort((x, y) => x.Artist.CompareTo(y.Artist));
+            SongsList.Sort((x, y) => x.Artist.CompareTo(y.Artist));                 //sorts the full library of songs by artist in alphabetical order
             DisplaySongs();
         }
 
         private static void SortByAlbum()
         {
-            SongsList.Sort((x, y) => x.Album.CompareTo(y.Album));
+            SongsList.Sort((x, y) => x.Album.CompareTo(y.Album));                   //sorts the full library of songs by album in alphabetical order
             DisplaySongs();
         }
 
@@ -191,6 +191,7 @@ namespace plmeredith13.CodeLou.FinalProject
             Console.WriteLine("1: Sort by song title");
             Console.WriteLine("2: Sort by artist");
             Console.WriteLine("3: Sort by album");
+            Console.WriteLine(" ");
         }
 
         private static void RemoveSongs()
@@ -201,14 +202,14 @@ namespace plmeredith13.CodeLou.FinalProject
             {
                 Console.WriteLine("\nEnter the title of the song you'd like to remove \n");
                 Key = Console.ReadLine();
-                var Songs = SongsList.Where(x => x.SongTitle.ToLower().Contains(Key.ToLower()));
-                DisplaySongs(Songs);
+                var Songs = SongsList.Where(x => x.SongTitle.ToLower().Contains(Key.ToLower()));        //once again, this filters the list correctly but...
+                DisplaySongs(Songs);                                                                    //...this STILL doesn't display the filtered list for some reason
                 
                 ConsoleKey response;
                 do
                 {
-                    Console.WriteLine("\nIs this the song you'd like to remove? [y/n] \n");
-                    response = Console.ReadKey(false).Key;
+                    Console.WriteLine("\nIs this the song you'd like to remove? [y/n] \n");         //this WHOLE method removes a song from the library and saves the file, while also making sure to ask a confirmation y/n question before removing the song
+                    response = Console.ReadKey(false).Key;                                          //surprisingly, even though the DisplaySongs(Songs) method doesn't correctly display the filtered list, it WILL delete the correct song from the library
                     if (response != ConsoleKey.Enter)
                     {
                         Console.WriteLine();
@@ -232,7 +233,7 @@ namespace plmeredith13.CodeLou.FinalProject
             var Song = new Song();
             Console.WriteLine("Enter song title \n");
             Song.SongTitle = Console.ReadLine();
-            Console.WriteLine("\nEnter song artist \n");
+            Console.WriteLine("\nEnter song artist \n");                        //adds a song to the library and then saves the file
             Song.Artist = Console.ReadLine();
             Console.WriteLine("\nEnter album name \n");
             Song.Album = Console.ReadLine();
@@ -244,7 +245,7 @@ namespace plmeredith13.CodeLou.FinalProject
 
         private static void DisplayMenu()
         {
-            Console.WriteLine("Select from the following operations:");
+            Console.WriteLine("Select from the following operations:");         //main menu for the console app
             Console.WriteLine("1: Enter new song");
             Console.WriteLine("2: List all songs");
             Console.WriteLine("3: Search for a song");
